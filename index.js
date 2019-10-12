02d8a401f5bf0d8673646ed64472f16397b67b11
free-writer-5650 <miles.higbie@gmail.com>
2019-10-12 17:01:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  const newKittens = [];
-  newKittens.push()
-  return newKittens
-}
 
-function destructivelyPrependKittenKitten(name) {
-  const newKittens = [];
-  newKittens.shift()
-  return newKittens
-}
 
 console.log(destructivelyPrependKitten("miles"))
